0ce230e78bbecb88e90e631b283096790de2ca1e
Tejas Amrit <97870241+tejasamrit@users.noreply.github.com>
2025-01-03 13:28:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Leaders/array-leaders.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Geeks {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine()); // Number of test cases
+        for (int g = 0; g < t; g++) {
+            String[] str =
+                (br.readLine()).trim().split(" "); // Reading input as a string array
+            int arr[] = new int[str.length]; // Creating integer array from the input
+            for (int i = 0; i < str.length; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+
+            // Getting the result from the Solution class
+            List<Integer> result = new Solution().leaders(arr);
+
+            // Printing the result in the required format
+            if (result.isEmpty()) {
+                System.out.println("[]");
+            } else {
+                for (int i = 0; i < result.size(); i++) {
+                    if (i != 0) System.out.print(" ");
+                    System.out.print(result.get(i));
+                }
+                System.out.println();    // Ensure new line after each test case output
+                System.out.println("~"); // Ensure new line after each test case output
+            }
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    static ArrayList<Integer> leaders(int arr[]) {
+        int n=arr.length;
+        ArrayList<Integer>list=new ArrayList<>();
+        int lead=arr[n-1];
+        list.add(arr[n-1]);
+        for(int i=n-2;i>=0;i--){
+            if(arr[i]>=lead){
+                lead=arr[i];
+                list.add(lead);
+            }
+        }
+        ArrayList<Integer>ans=new ArrayList<>();
+        int lsize=list.size();
+        for(int i=lsize-1;i>=0;i--){
+            ans.add(list.get(i));
+        }
+        return ans;
+    }
+}
