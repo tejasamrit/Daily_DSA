405589ada179c7f3451302376bd91a2e30e6f85b
Tejas Amrit <97870241+tejasamrit@users.noreply.github.com>
2025-01-05 01:13:30 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (33.32%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,14 +1,12 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int[n + 1];
-  
-        // Base cases
-        dp[0] = 1;
-        dp[1] = 1;
-
-        for (int i = 2; i <= n; i++)
-            dp[i] = dp[i - 1] + dp[i - 2]; 
-  
-        return dp[n];
+        int p1=1;
+        int p2=1;
+        for(int i=2;i<=n;i++){
+            int curr=p1+p2;
+            p2=p1;
+            p1=curr;
+        }
+        return p1;
     }
 }
