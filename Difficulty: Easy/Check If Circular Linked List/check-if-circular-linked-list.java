f60501f219542d7458d07990993e4d9ae93c8dbb
Tejas Amrit <97870241+tejasamrit@users.noreply.github.com>
2025-01-04 14:13:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check If Circular Linked List/check-if-circular-linked-list.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+public class LinkedList {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        scanner.nextLine(); // Ignore the newline character after t
+
+        for (int testCase = 0; testCase < t; testCase++) {
+            int k = scanner.nextInt();
+            scanner.nextLine(); // Ignore the newline character after k
+
+            String inputLine = scanner.nextLine().trim();
+            String[] inputArray = inputLine.split(" ");
+
+            Node head = null, tail = null;
+            if (inputArray.length > 0 && !inputArray[0].isEmpty()) {
+                head = new Node(Integer.parseInt(inputArray[0]));
+                tail = head;
+
+                for (int i = 1; i < inputArray.length; i++) {
+                    if (!inputArray[i].isEmpty()) {
+                        tail.next = new Node(Integer.parseInt(inputArray[i]));
+                        tail = tail.next;
+                    }
+                }
+
+                if (k == 1 && head != null) {
+                    tail.next = head;
+                }
+            }
+
+            Solution solution = new Solution();
+            boolean isCircular = solution.isCircular(head);
+            System.out.println(isCircular ? "true" : "false");
+        
+System.out.println("~");
+}
+        scanner.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    boolean isCircular(Node head) {
+        Node slow=head;
+        Node fast=head;
+        
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) return true;
+        }
+        
+        return false;
+    }
+}
