a393ddf23b43ffe918b36e3fafa523fc25079a94
Tejas Amrit <97870241+tejasamrit@users.noreply.github.com>
2025-01-18 21:31:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/First Set Bit/first-set-bit.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    
+    
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();//taking testcases
+		while(t-->0){
+			int n=sc.nextInt();//input n
+			Solution ob=new Solution();
+			System.out.println(ob.getFirstSetBit(n));//calling method
+		
+System.out.println("~");
+}
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to find position of first set bit in the given number.
+    public static int getFirstSetBit(int n){
+        int temp=n;
+        int count=1;
+        while((n&1)==0){
+                count++;
+                n=n>>1;
+            
+        }
+        return count;    
+    }
+}
