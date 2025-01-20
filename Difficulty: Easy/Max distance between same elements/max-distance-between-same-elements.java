7174141557a8f2250fb0f518b7d98f1317feeda5
Tejas Amrit <97870241+tejasamrit@users.noreply.github.com>
2025-01-20 13:25:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Max distance between same elements/max-distance-between-same-elements.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.util.Scanner;
+
+
+// } Driver Code Ends
+class Solution {
+    public int maxDistance(int[] arr) {
+         // Code here
+        HashMap<Integer,Integer> mp = new HashMap<>();
+        int s=0;
+        for(int i=0;i<arr.length;i++) {
+            if(!mp.containsKey(arr[i])) {
+                mp.put(arr[i],i);
+            }
+            else {
+                s=Math.max(s,i-mp.get(arr[i]));
+            }
+        }
+        return s;
+    }
+}
+
+//{ Driver Code Starts.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine();
+        while (t-- > 0) {
+            String input = sc.nextLine();
+            String[] parts = input.split(" ");
+            int[] arr = new int[parts.length];
+            for (int i = 0; i < parts.length; i++) {
+                arr[i] = Integer.parseInt(parts[i]);
+            }
+            Solution ob = new Solution();
+            System.out.println(ob.maxDistance(arr));
+        
+System.out.println("~");
+}
+        sc.close();
+    }
+}
+// } Driver Code Ends
