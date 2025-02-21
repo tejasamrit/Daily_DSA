7c634f82978f329a6204a702b9f15d3e70aaafc6
Tejas Amrit <97870241+tejasamrit@users.noreply.github.com>
2025-02-21 12:58:41 +0530

    Time: 4 ms (94.64%) | Memory: 59.9 MB (28.59%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
@@ -1,28 +1,16 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] arr) {
-        ArrayList<Integer>list=new ArrayList<>();
-        cycleSort(arr);
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]!=i+1){
-                list.add(i+1);
+        int n = arr.length;
+        boolean[] visit = new boolean[n + 1];
+        for (int i : arr) {
+            visit[i] = true;
+        }
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (!visit[i]) {
+                ans.add(i);
             }
         }
-        return list;
-    }
-    public void cycleSort(int arr[]){
-        int n=arr.length;
-        int i=0;
-        while(i<n){
-            int ele=arr[i];
-            int correctPosition= ele-1;
-            if(arr[i]!=arr[correctPosition]){
-                swap(arr,i,correctPosition);
-            }else{
-                i++;
-            }
-        }
-    }
-    public void swap(int arr[],int i,int j){
-        int temp=arr[i]; arr[i]=arr[j]; arr[j]=temp;
+        return ans;
     }
 }
