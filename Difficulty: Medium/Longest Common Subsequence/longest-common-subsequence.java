f22abcc91b0ef299a8f6292d79b96c3cab29cd8f
Tejas Amrit <97870241+tejasamrit@users.noreply.github.com>
2025-03-06 11:22:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Common Subsequence/longest-common-subsequence.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
+        while (test-- > 0) {
+            String s1 = sc.next(); // Take both the strings as input
+            String s2 = sc.next();
+
+            Solution obj = new Solution();
+
+            // Call the lcs function with the lengths of the strings as
+            // parameters
+            System.out.println(obj.lcs(s1, s2));
+            System.out.println("~");
+        }
+        sc.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    static int lcs(String s1, String s2) {
+        int dp[][]=new int[s1.length()+1][s2.length()+1];
+        for(int i=1;i<=s1.length();i++){
+            for(int j=1;j<=s2.length();j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j]= 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[s1.length()][s2.length()];
+    }
+}
